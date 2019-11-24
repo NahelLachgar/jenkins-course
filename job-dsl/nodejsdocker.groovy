a0bacdaade479c3cf2b21b28d451852d7bace575
Nahel Lachgar <nahellachgar@hotmail.fr>
2019-11-24 18:32:43 +0100

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('NahelLachgar/docker-nodejs-demo')
+            repositoryName('nahelLachgar/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
